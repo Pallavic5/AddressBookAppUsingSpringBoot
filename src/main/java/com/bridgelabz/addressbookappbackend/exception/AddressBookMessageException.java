59884857f8243cc9dafc1f72d6196cab2d03cf85
Pallavic5 <pchedge235@gmail.com>
2023-02-24 14:31:37 +0530

    Added:Ability to create exception layer

---
+++ src/main/java/com/bridgelabz/addressbookappbackend/exception/AddressBookMessageException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookappbackend.exception;
+
+public class AddressBookMessageException extends RuntimeException{
+    public AddressBookMessageException(String message){
+        super(message);
+    }
+}
